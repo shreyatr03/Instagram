b9e738d5a6498cfbf83bac386a7e97ce00e77f7b
shreya <shreyatr@outlook.com>
2021-10-09 13:01:11 +0530

    commit

--- helper/connection.go
+++ helper/connection.go
@@ -2,52 +2,23 @@ package helper
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
 	"log"
-	"net/http"
+	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func ConnectDB() *mongo.Collection {
+func ConnectDB() (*mongo.Client, context.Context) {
 
 	// Set client options
 	clientOptions := options.Client().ApplyURI("mongodb+srv://admin:admin@cluster0.hvyie.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 
-	// Connect to MongoDB
-	client, err := mongo.Connect(context.TODO(), clientOptions)
-
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	fmt.Println("Connected to MongoDB!")
-
-	collection := client.Database("go_rest_api").Collection("books")
-
-	return collection
-}
-
-// ErrorResponse : This is error model.
-type ErrorResponse struct {
-	StatusCode   int    `json:"status"`
-	ErrorMessage string `json:"message"`
-}
-
-// GetError : This is helper function to prepare error model.
-// If you want to export your function. You must to start upper case function name. Otherwise you won't see your function when you import that on other class.
-func GetError(err error, w http.ResponseWriter) {
-
-	log.Fatal(err.Error())
-	var response = ErrorResponse{
-		ErrorMessage: err.Error(),
-		StatusCode:   http.StatusInternalServerError,
-	}
-
-	message, _ := json.Marshal(response)
-
-	w.WriteHeader(response.StatusCode)
-	w.Write(message)
+	return client, ctx
 }
--- main.go
+++ main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"context"
+	"fmt"
 	"log"
 	"net/http"
-	"time"
 
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"github.com/shreyatr03/Instagram/helper"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func usersHandlers(w http.ResponseWriter, r *http.Request) {
@@ -26,31 +25,31 @@ func usersHandlers(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-
-	clientOptions := options.Client().
-		ApplyURI("mongodb+srv://admin:admin@cluster0.hvyie.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	client, err := mongo.Connect(ctx, clientOptions)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer client.Disconnect(ctx)
-
-	// instaDatabase := client.Database("instadb")
-	// usersCollection := instaDatabase.Collection("user")
-	// // postsCollection := instaDatabase.Collection("posts")
-
-	// userResult, err := usersCollection.InsertOne(ctx, bson.D{
-	// 	{Key: "name", Value: "Shreya"},
-	// 	{Key: "email", Value: "shreyatr@outlook.com"},
-	// 	{Key: "password", Value: "pass"},
-	// })
+	//user := models.User{}
+	// clientOptions := options.Client().
+	// 	ApplyURI("mongodb+srv://admin:admin@cluster0.hvyie.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+	// ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	// defer cancel()
+	// client, err := mongo.Connect(ctx, clientOptions)
 	// if err != nil {
 	// 	log.Fatal(err)
 	// }
-	// fmt.Println(userResult.InsertedID)
+	client, ctx := helper.ConnectDB()
+	defer client.Disconnect(ctx)
+
+	instaDatabase := client.Database("instadb")
+	usersCollection := instaDatabase.Collection("user")
+	// postsCollection := instaDatabase.Collection("posts")
+
+	userResult, err := usersCollection.InsertOne(ctx, bson.D{
+		{Key: "name", Value: "Shreya_1"},
+		{Key: "email", Value: "shreyatr@outlook.com"},
+		{Key: "password", Value: "pass"},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(userResult.InsertedID)
 
 	http.HandleFunc("/users/{id}", usersHandlers)
 
